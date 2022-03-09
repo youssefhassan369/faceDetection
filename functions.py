d1d57018b3a9d9c87e897251f196fc0658107b83
youssef hassan <youssefhassan369@yahoo.com>
2022-03-09 17:19:56 +0200

    splitting datamatrix to train and test is done

--- functions.py
+++ functions.py
@@ -16,9 +16,16 @@ def load(folder):
 
     y = np.array(y)
     target_names = []
-    print(y.shape)
-    print(y)
     for i in range(1, 41):
         name = ''
         name += 's' + str(i)
         target_names.append(name)
+    return y
+
+
+def split(data):
+    train = data[1::2]  # Elements from list1 starting from 1 iterating by 2
+    test = data[::2]  # Elements from list1 starting from 0 iterating by 2
+    test = np.array(test)
+    train = np.array(train)
+    return train, test
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-from functions import load
+from functions import load, split
 
 if __name__ == '__main__':
-    load('Dataset')
+    data = load('Dataset')
+    split(data)
