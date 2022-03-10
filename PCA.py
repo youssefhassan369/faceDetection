a709522ba61552f30239fcfae7efcf5b38b71b67
youssef hassan <youssefhassan369@yahoo.com>
2022-03-10 18:15:50 +0200

    covariance function is done

--- PCA.py
+++ PCA.py
@@ -1,10 +1,19 @@
 import numpy as np
 
 
-def func(train):
+def covariance(train):
+    train = np.array(train)
     mean = train.mean(axis=0)
     mean = np.array(mean)
-    centered_data = train - np.dot(1, mean.transpose())
-    centered_data=np.array(centered_data)
-    covariance = np.dot(centered_data, centered_data.transpose())
-    print(mean)
+    centered_data = train - mean.transpose()
+    centered_data = np.array(centered_data)
+    covariance = (np.dot(centered_data.transpose(), centered_data)) / train.shape[0]
+    return covariance
+
+
+def PCA(train):
+    cov = covariance(train)
+    eigen_val, eigen_vect = np.linalg.eig(cov)
+    print(eigen_val)
+    print(eigen_vect)
+
