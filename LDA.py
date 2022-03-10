69fe845ac5c50bc1029cd4f253a8c50b0739581e
Nour El-din Hazem <nourhazem16@gmail.com>
2022-03-10 18:18:05 +0200

    version 2

--- LDA.py
+++ LDA.py
@@ -1,6 +1,23 @@
 import numpy as np
 
+
 def LDA(train):
-    meanVector=train.mean(axis=0)
-    meanVector=np.array(meanVector)
-    print(meanVector);
+    mean = []
+    for i in range(1, 41):
+        temp =[]
+        for j in range(((i - 1) * 5), (i * 5) ):
+            temp.append(train[j])
+        temp=np.array(temp)
+        meanVector = temp.mean(axis=0)
+        meanVector = np.array(meanVector)
+        mean.append(meanVector)
+
+    mean = np.array(mean)
+    temp = np.array(temp)
+    print(mean.shape)
+
+    overall_mean = mean.mean(axis=0)
+    print(overall_mean.shape)
+
+
+
